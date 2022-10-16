e83e8328ee64aa76b9c477a0a87c9673f93e1b11
usmankhurshid28 <usmankhurshidofficial@gmail.com>
2022-10-16 22:34:03 +0500

    searchfiled

--- UnitTestProject3/searchfunctionality.cs
+++ UnitTestProject3/searchfunctionality.cs
@@ -17,7 +17,7 @@ namespace UnitTestProject3
         By search_button = By.XPath("//input[@value='Search']");
         By bumper = By.XPath("//a[text()=' Honda Vezel 2014-21 Front Bumper ']");
         By add_bumper_to_cart = By.Id("add-to-cart-button-4635");
-        By shoping_cart = By.XPath("//*[@id='bar - notification']/div/span");
+        By shoping_cart = By.XPath("//a[text()='shopping cart']");
 
 
         public void searchfunc()
@@ -30,7 +30,7 @@ namespace UnitTestProject3
             scrolltoelement_click(search_button);
             scrolltoelement_click(bumper);
             scrolltoelement_click(add_bumper_to_cart);
-            scrolltoelement_click(shoping_cart);
+            clickable_items(shoping_cart);
         }
     }
 }
